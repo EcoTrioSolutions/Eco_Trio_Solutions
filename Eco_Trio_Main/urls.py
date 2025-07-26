80c052572f5beb6474046505f0bfc5c27ff95cbd
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-27 02:37:03 +0530

    New upload add n

--- Eco_Trio_Main/urls.py
+++ Eco_Trio_Main/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return HttpResponse("🚀 Hello from Eco_Trio_Main!")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('', include('Eco_Trio_Sub.urls')),
 ]
 
